5b6e6791dc85d0a926ddcba0824d5b4489f306a7
misarb <lahssane12@gmail.com>
2022-11-09 22:21:33 +0100

    homogene transformation denavit hartenberg

--- TP1/matricHomgDenavit.m
+++ TP1/matricHomgDenavit.m
@@ -1,6 +1,11 @@
+%% Transformation Matrices
 function [matrice] = matricHomgDenavit(mR,t)
 
 
-matric = [mR t ;0 0 0 1]
+matrice = [mR(1,1) mR(1,2) mR(1,3) t(1) ;mR(2,1) mR(2,2) mR(2,3) t(2) ;mR(3,1) mR(3,2) mR(3,3) t(3) ; 0 0 0 1];
     
 end
+
+
+
+   
